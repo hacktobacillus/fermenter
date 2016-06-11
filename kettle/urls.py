644360c7c050edc545859d226a4d85807d84412a
Jason Slay <jason.t.slay@gmail.com>
2016-06-11 03:51:21 -0600

    one more commit

---
+++ kettle/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^beer_list$', views.beer_list, name='beer_list'),
+]

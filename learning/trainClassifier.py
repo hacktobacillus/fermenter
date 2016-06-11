b4fdf0a51e99ca81cee0dcb2ea20bdbe9d079fe9
Daniel Wilson <harenil@gmail.com>
2016-06-11 03:51:23 -0600

    changed inheritance structure on the classifier

--- learning/trainClassifier.py
+++ learning/trainClassifier.py
@@ -1,30 +1,47 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 from formatData import BeerMLData
 import numpy as np
 
-class LRBeerClassifier:
+
+class Classifier:
     def __init__(self):
-        self.regularization = 100.0
-        self.classifier = LogisticRegression(C=self.regularization)
         self.beer_data = BeerMLData()
         self.beer_data.from_model()
         self.beer_data.create_beer_mapping()
         self.beer_data.get_mapping_asarray()
 
-    def train(self,pos,neg):
-
+    def get_features(self,pos,neg):
         pos = [self.beer_data.map_beer(beer) for beer in pos]
         neg = [self.beer_data.map_beer(beer) for beer in neg]
        
 
         X = np.vstack((np.array(pos),np.array(neg)))
         y = np.concatenate((np.ones(len(pos)),np.zeros(len(neg))))
+        return X,y
 
+    def train(self,pos,neg):
+        X,y = self.get_features(pos,neg)
         self.classifier.fit(X,y)
 
     def classify(self,beer_id):
         mapped = self.beer_data.map_beer(beer_id)
-        return self.classifier.predict_proba(np.array([mapped]))
+        return self.classifier.predict_proba(np.array([mapped]))[0][1]
+ 
+class NBBeerClassifier(Classifier):
+
+    def __init__(self):
+        Classifier.__init__(self)
+        self.classifier = GaussianNB()
+
+class LRBeerClassifier(Classifier):
+
+    def __init__(self,regularization=100.0):
+        Classifier.__init__(self)
+        self.regularization = regularization
+        self.classifier = LogisticRegression(C=self.regularization)
+
+
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -35,7 +52,8 @@ if __name__ == '__main__':
 
     
 
-    classifier = LRBeerClassifier()
+    #classifier = LRBeerClassifier()
+    classifier = NBBeerClassifier()
     classifier.train(beers_i_like,beers_i_dont)
 
     
@@ -46,7 +64,7 @@ if __name__ == '__main__':
 
     for i,beer in enumerate(classifier.beer_data):
         k = beer['name']
-        val = classifier.classify(beer['id'])[0][1]
+        val = classifier.classify(beer['id'])
         print (k,val)
 
         color = jet(int(255*val))

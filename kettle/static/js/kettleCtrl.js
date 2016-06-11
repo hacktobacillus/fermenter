3a0b6b31a0b3c5379222879e7ce6d0839d6300c1
Daniel Wilson <harenil@gmail.com>
2016-06-11 12:09:39 -0600

    added like/dislike functions

--- kettle/static/js/kettleCtrl.js
+++ kettle/static/js/kettleCtrl.js
@@ -4,9 +4,15 @@ angular.module("kettleApp")
     $scope.model = {
         beer_list: []
     }
-    $scope.like_beers = ['samael-s'];
-    $scope.dont_beers = ['ipa'];
+
+    
+
+    $scope.like_beers = [];
+    $scope.dislike_beers = [];
     $scope.postBeers = postBeers;
+    $scope.downVoteBeer = downVoteBeer;
+    $scope.upVoteBeer = upVoteBeer;
+
 
     $http.get('/kettle/beer_list').then(
         function(data) {
@@ -15,6 +21,65 @@ angular.module("kettleApp")
         },
         function () {}
     );
+
+
+    function upVoteBeer(beer) {
+        // return if this beer has been voted on.
+        if (beer.voted_on) {
+            return;
+        }
+        var beer_exists = false;
+        for (var i in $scope.like_beers) {
+            var bid  = $scope.like_beers[i];
+            if (beer.id = bid) {
+                beer_exists = true;
+                break
+            }
+        }
+        if (!beer_exists) {
+            $scope.like_beers.push(beer.id);
+        }
+
+        // Make sure it's not in dislike beers.
+        for (var i in $scope.dislike_beers) {
+            var bid  = $scope.dislike_beers[i];
+            // remove it if present
+            if (beer.id = bid) {
+                $scope.dislike_beers.splice(i,1);
+                beer.voted_on = false;
+                return;
+        }
+        beer.voted_on = true;
+    }
+
+    function downVoteBeer(beer) {
+        // return if this beer has been voted on.
+        if (beer.voted_on) {
+            return;
+        }
+        var beer_exists = false;
+        for (var i in $scope.dislike_beers) {
+            var bid  = $scope.dislike_beers[i];
+            if (beer.id = bid) {
+                beer_exists = true;
+                break
+            }
+        }
+        if (!beer_exists) {
+            $scope.dislike_beers.push(beer.id);
+        }
+
+        // Make sure it's not in like beers.
+        for (var i in $scope.like_beers) {
+            var bid  = $scope.like_beers[i];
+            // remove it if present
+            if (beer.id = bid) {
+                $scope.like_beers.splice(i,1);
+                beer.voted_on = false;
+                return;
+        }
+        beer.voted_on = true;
+ }
 
 
     function postBeers() {

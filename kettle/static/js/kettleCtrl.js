ea283adcf1a1b3b29e12f2417f1346354b9385dc
Daniel Wilson <harenil@gmail.com>
2016-06-11 10:42:37 -0600

    changes to djangularify it

--- kettle/static/js/kettleCtrl.js
+++ kettle/static/js/kettleCtrl.js
@@ -1,14 +1,17 @@
 (function() {
 angular.module("kettleApp")
 .controller("kettleCtrl",function($scope,$http) {
-
+    $scope.model = {
+        beer_list: []
+    }
     $scope.like_beers = ['samael-s'];
     $scope.dont_beers = ['ipa'];
     $scope.postBeers = postBeers;
 
     $http.get('/kettle/beer_list').then(
         function(data) {
-            console.log(JSON.parse(data.data));
+            $scope.model.beer_list = data.data.result;
+            console.log($scope.model.beer_list);
         },
         function () {}
     );
@@ -18,7 +21,7 @@ angular.module("kettleApp")
         $http.post('/kettle/crunch',{like_ids:$scope.like_beers,dislike_ids:$scope.dont_beers}).then(
         function (data) { 
             console.log(data);
-            alert('success!');
+            alert('<img src="http://http://media.giphy.com/media/rnR8L2AtvBpD2/giphy-tumblr.gif"></img>');
         },
         function () { 
             alert('failed');
